15a33dd4a365d6edcebc22576e7f622b7de697cd
DeiuriRiccardo <riccardo.deiuri>
2023-09-15 15:02:38 +0200

    implementazione parole orizzontali nella matrice

--- 5_Applicativo/script.js
+++ 5_Applicativo/script.js
@@ -1,7 +1,5 @@
-
-var difficolta = document.getElementById("difficolta").value;
-
-function generaTabella(){
+function elaboraDifficolta(){
+    var difficolta = document.getElementById("difficolta").value;
     var itable = 0;
     var ntable = 0;
     switch(difficolta){
@@ -22,6 +20,11 @@ function generaTabella(){
             ntable = 20;
             break;
     }
+    return [itable, ntable];
+}
+
+function generaTabella(){
+    var [itable, ntable] = elaboraDifficolta();
     var table = "<table>";
     for (let i = 0; i < itable; i++) {
         table += "<tr>";
@@ -35,12 +38,63 @@ function generaTabella(){
 }
 
 function popolaTabella() {
-    var parole;
-    // tramite ajax leggo il contnuto del dizionario
-    $.get('280000_parole_italiane.txt', function(file) {
-        $.each(parola => {
-            parole += parola;
-        });
-    });
-    console.log(parole);
+    /*var parole;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        console.log(this.responseText[i]);
+    }
+    xhttp.open("GET", "280000_parole_italiane.txt");
+    xhttp.send();
+
+
+    console.log(parole);*/
+    var [x, y] = elaboraDifficolta();
+    var parole = ["ciao", "abbagliava", "pippa", "zuppa", "fascia",
+                    "prova", "buongiorno", "ciupilan"]
+    var contenuto = [x];
+
+    for (let i = 0; i < x; i++) {
+    contenuto[i] = new Array(y);
+    }
+    
+    // orizzontale
+    for (let j = 0; j < parole.length; j++) {
+        var recalc = false;
+        var lettere = parole[j].split("");
+        // deve iniziare in una posiziona in cui ci stia tutta la parola 
+        var posX = parseInt(Math.random()*(y-lettere.length));
+        var posY = parseInt(Math.random()*x);
+        console.log( posX +" "+ posY);
+        for (let m = 0; m < lettere.length; m++) {
+            for (let i = 0; i < contenuto.length; i++) {
+                for (let n = 0; n < contenuto[i].length; n++) {
+                    
+                    if(i == posY && n == posX){
+                        // non si deve sovrapporre
+                        if (contenuto[i][n] == null) {
+                             contenuto[i][n] = lettere[m];
+                             
+                        }else{
+                            for (let g = m; g > 0; g--) {
+                                contenuto[i][n] = null;
+                                n--;
+                                recalc = true;
+                                
+                            }
+                        }
+                    }
+                
+                }
+            }
+            if(recalc){
+                j--;
+                break;
+            }
+            posX++;
+        }
+        
+    }
+    console.log(contenuto);
 }
+
+
