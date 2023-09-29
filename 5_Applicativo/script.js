7824fa8a2a112711bb2ca24a023a24882db05fbb
DeiuriRiccardo <96241842+DeiuriRiccardo@users.noreply.github.com>
2023-09-29 15:40:55 +0200

    Add files via upload
    Implementazione dizionario

--- 5_Applicativo/script.js
+++ 5_Applicativo/script.js
@@ -1,26 +1,54 @@
-function elaboraDifficolta(){
+function elaboraDifficolta(option){
     var difficolta = document.getElementById("difficolta").value;
-    var itable = 0;
-    var ntable = 0;
-    switch(difficolta){
-        case "principiante":
-            itable = 10;
-            ntable = 10;
-            break;
-        case "dilettante":
-            itable = 12;
-            ntable = 10;
-            break;
-        case "esperto":
-            itable = 15;
-            ntable = 10;
-            break;
-        case "leggenda":
-            itable = 20;
-            ntable = 20;
-            break;
+    if(option == "column"){
+        var column = 2;
+        if(difficolta == "leggenda"){
+            column = 3;
+        }else if(difficolta != "principiante"){
+            column = 3;
+        }
+        return column;
+    }else if(option == "parole"){
+        var num = 0;
+        switch(difficolta){
+            case "principiante":
+                num = 7;
+                break;
+            case "dilettante":
+                num = 10;
+                break;
+            case "esperto":
+                num = 15;
+                break;
+            case "leggenda":
+                num = 25;
+                break;
+        }
+        return num;
+    }else{
+        var itable = 0;
+        var ntable = 0;
+        switch(difficolta){
+            case "principiante":
+                itable = 10;
+                ntable = 10;
+                break;
+            case "dilettante":
+                itable = 10;
+                ntable = 14;
+                break;
+            case "esperto":
+                itable = 12;
+                ntable = 15;
+                break;
+            case "leggenda":
+                itable = 18;
+                ntable = 20;
+                break;
+        }
+        return [itable, ntable];
     }
-    return [itable, ntable];
+    
 }
 
 function generaTabella(){
@@ -36,42 +64,92 @@ function generaTabella(){
     var tabella = document.getElementById("table");
     tabella.innerHTML = table;
     tabella.style.borderWidth = "2px";
-    popolaTabella();
+
+    generaParole();
 }
 
-function popolaTabella() {
-    /*var parole;
-    var xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-        console.log(this.responseText[i]);
-    }
-    xhttp.open("GET", "280000_parole_italiane.txt");
-    xhttp.send();
+function generaParole(){
+    var parole = [];
+    leggo().then(value => {
+        for (let i = 0; i < value.length; i++) {
+            parole.push(value[i]);
+        }
+        popolaTabella(parole);
+        stampaLista(parole);
+    });
+}
+
+function leggiContenuto(){
+    return new Promise((resolve) => {
+        var input = document.getElementById('fileInput');
+        var file = input.files[0];
+        var reader = new FileReader();
+        var numeroParole = elaboraDifficolta("parole");
+        var [x, y] = elaboraDifficolta();
+        var maxChar = Math.min(x, y);
+        var parole = [];
+        reader.onload = function() {
+            var testo = reader.result;
+            var paroleFile = testo.split('\n');
+            
+            for (let i = 0; i < numeroParole; i++){
+                var parola = paroleFile[parseInt(Math.random()*paroleFile.length)];
+                if(parola.length > 2 && parola.length <= maxChar){
+                    parole[i] = parola;
+                }else{
+                    i--;
+                }
+            }
+            console.log(parole);
+            console.log(numeroParole);
+            resolve(parole);
+        };
+        reader.readAsText(file);
+    })
+}
+
+async function leggo(){
+    const x = await leggiContenuto();
+    return x;
+}
+    
 
 
-    console.log(parole);*/
+function popolaTabella(parole) {
     var [altezza, larghezza] = elaboraDifficolta();
-    var parole = ["ciao", "abbagliava", "pippa", "zuppa", "fascia",
-                    "prova", "ciupilan"]
+    
     var contenuto = [altezza];
+    svuotaTabella();
 
-    for (let i = 0; i < altezza; i++) {
-        contenuto[i] = new Array(larghezza);
-        for (let n = 0; n < larghezza; n++) {
-            contenuto[i][n] = null;
+    function svuotaTabella(){
+        for (let i = 0; i < altezza; i++) {
+            contenuto[i] = new Array(larghezza);
+            for (let n = 0; n < larghezza; n++) {
+                contenuto[i][n] = null;
+            }
         }
     }
     
-    
+    var interrupt = 0;
     for (let j = 0; j < parole.length; j++) {
+        /* interrupt serve a interrompere i cicli e rigenerare le parole
+           nel caso in cui si generasse un ciclo infinito */
+        interrupt++;
+        if(interrupt > 1000){
+            interrupt = 0;
+            svuotaTabella();
+            generaParole();
+
+        }
+        console.log(interrupt);
         var recalc = false;
         var lettere = parole[j].toUpperCase().split("");
         var direction = parseInt(Math.random()*8);
-
         if(direction == 0 || direction == 1){ // orizzontale
             // deve iniziare in una posiziona in cui ci stia tutta la parola 
             var posX = parseInt(Math.random()*(larghezza+1-lettere.length));
             var posY = parseInt(Math.random()*altezza);
+            console.log( `${parole[j]} ${direction} ${posX}:${posY}`);
             var memoryX = -1;
             if(direction == 1){ // invertito
                 lettere.reverse();
@@ -92,6 +170,7 @@ function popolaTabella() {
                                 if(m == 0){
                                     recalc = true;
                                 }else{
+                                    // cancella le lettere aggiunte fino a quel momento
                                     for (let g = m; g > 0; g--) {
                                         n--;
                                         if(memoryX != n){
@@ -116,7 +195,7 @@ function popolaTabella() {
             // deve iniziare in una posiziona in cui ci stia tutta la parola 
             var posX = parseInt(Math.random()*larghezza);
             var posY = parseInt(Math.random()*(altezza+1-lettere.length));
-            var memoryY = -1;
+            console.log( `${parole[j]} ${direction} ${posX}:${posY}`);            var memoryY = -1;
             if(direction == 3){ // invertito
                 lettere.reverse();
             }
@@ -163,7 +242,7 @@ function popolaTabella() {
             // deve iniziare in una posiziona in cui ci stia tutta la parola 
             var posX = parseInt(Math.random()*(larghezza+1-lettere.length));
             var posY = parseInt(Math.random()*(altezza+1-lettere.length));
-            var memoryX = -1;
+            console.log( `${parole[j]} ${direction} ${posX}:${posY}`);            var memoryX = -1;
             var memoryY = -1;
             if(direction == 5){ // invertito
                 lettere.reverse();
@@ -212,9 +291,9 @@ function popolaTabella() {
             }
         }else if(direction == 6 || direction == 7){ // diagonale verso sinistra
             // deve iniziare in una posiziona in cui ci stia tutta la parola 
-            var posX = lettere.length-1 + parseInt(Math.random());
+            var posX = lettere.length-1 + parseInt(Math.random()*(larghezza-lettere.length));
             var posY = parseInt(Math.random()*(altezza+1-lettere.length));
-            var memoryX = -1;
+            console.log( `${parole[j]} ${direction} ${posX}:${posY}`);            var memoryX = -1;
             var memoryY = -1;
             if(direction == 7){ // invertito
                 lettere.reverse();
@@ -277,10 +356,20 @@ function popolaTabella() {
         for (let n = 0; n < contenuto[i].length; n++) {
             if(contenuto[i][n] == null){
                 contenuto[i][n] = alfabeto[parseInt(Math.random()*alfabeto.length)];
+                td[n].style.color = "red";
+            }else{
+                td[n].style.color = "black";
             }
             td[n].textContent = contenuto[i][n];
         }
     }
 }
 
-
+function stampaLista(parole){
+    var list = document.getElementById('list');
+    list.innerHTML = "";
+    list.style.columnCount = elaboraDifficolta("column");
+    for (let j = 0; j < parole.length; j++) {
+        list.innerHTML += parole[j] + "<br>";
+    }
+}
